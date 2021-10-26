75b95de409d8f8864185aab3db00e3db32a09ae0
duanluan <duanluan@outlook.com>
2021-10-26 23:43:25 +0800

    feat: 完善集合工具类

--- src/main/java/top/zhogjianhao/CollectionUtils.java
+++ src/main/java/top/zhogjianhao/CollectionUtils.java
@@ -1,16 +1,72 @@
 package top.zhogjianhao;
 
 
+import java.util.Collection;
+
+/**
+ * 集合工具类
+ *
+ * @author duanluan
+ */
 public class CollectionUtils {
 
-  private CollectionUtils(){
+  /**
+   * 为 null 或没有元素
+   *
+   * @param coll
+   * @return
+   */
+  public static boolean isEmpty(Collection<?> coll) {
+    return org.apache.commons.collections4.CollectionUtils.isEmpty(coll);
   }
 
-  public static boolean sizeIsEmpty(Object object) {
-    return org.apache.commons.collections4.CollectionUtils.sizeIsEmpty(object);
+  /**
+   * 不为 null 且有元素
+   *
+   * @param coll
+   * @return
+   */
+  public static boolean isNotEmpty(Collection<?> coll) {
+    return org.apache.commons.collections4.CollectionUtils.isNotEmpty(coll);
   }
 
-  public static boolean sizeIsNotEmpty(Object object) {
-    return !org.apache.commons.collections4.CollectionUtils.sizeIsEmpty(object);
+  /**
+   * 不为 null 但内容为空
+   *
+   * @param obj
+   * @return
+   */
+  public static boolean sizeIsEmpty(Object obj) {
+    return org.apache.commons.collections4.CollectionUtils.sizeIsEmpty(obj);
+  }
+
+  /**
+   * 不为 null 且内容不为空
+   *
+   * @param obj
+   * @return
+   */
+  public static boolean sizeIsNotEmpty(Object obj) {
+    return !org.apache.commons.collections4.CollectionUtils.sizeIsEmpty(obj);
+  }
+
+  /**
+   * 不为 null 但只有空元素
+   *
+   * @param coll
+   * @return
+   */
+  public static boolean allIsEmpty(Collection<?> coll) {
+    return org.apache.commons.collections4.CollectionUtils.removeAll(coll, null).size() == 0;
+  }
+
+  /**
+   * 不为 null 且不只有空元素
+   *
+   * @param coll
+   * @return
+   */
+  public static boolean allIsNotEmpty(Collection<?> coll) {
+    return org.apache.commons.collections4.CollectionUtils.removeAll(coll, null).size() > 0;
   }
 }
