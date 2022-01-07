6c4e30669e353eb8c338a745bd6b9274dbf12b7a
wangjiyang <291603977@qq.com>
2022-01-07 17:15:04 +0800

    --author="wjy" 新增加密工具类

---
+++ src/main/java/top/zhogjianhao/DESUtil.java
@@ -0,0 +1,137 @@
+package top.zhogjianhao;
+
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * 加密工具类
+ *
+ * @author wjy
+ */
+public class DESUtil {
+
+    private final static String DES = "DES";
+    private final static String ENCODE = "GBK";
+    private final static String defaultKey = "abs";
+
+    /**
+     * 使用 默认key 加密
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String data) throws Exception {
+        byte[] bt = encrypt(data.getBytes(ENCODE), defaultKey.getBytes(ENCODE));
+        String strs = Base64.getEncoder().encodeToString(bt);
+        return strs;
+    }
+
+    /**
+     * 使用 默认key 解密
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    public static String decrypt(String data) throws IOException, Exception {
+        if (data == null) {
+            return null;
+        }
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] buf = decoder.decode(data);
+        byte[] bt = decrypt(buf, defaultKey.getBytes(ENCODE));
+        return new String(bt, ENCODE);
+    }
+
+    /**
+     * 指定密钥加密
+     * @param data  加密数据
+     * @param key   指定的加密Key
+     * @return      密文
+     **/
+    public static String encryptAppointKey(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes(ENCODE), key.getBytes(ENCODE));
+        return new BASE64Encoder().encode(bt);
+    }
+
+    /**
+     * Description 根据键值进行解密
+     * @param data
+     * @param key
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    public static String decrypt(String data, String key) throws IOException,
+            Exception {
+        if (data == null) {
+            return null;
+        }
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] buf = decoder.decode(data);
+        byte[] bt = decrypt(buf, key.getBytes(ENCODE));
+        return new String(bt, ENCODE);
+    }
+
+    /**
+     * Description 根据键值进行加密
+     * @param data
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * Description 根据键值进行解密
+     * @param data
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+}
