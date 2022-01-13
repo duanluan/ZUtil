28351747bf205e7f7c2c226ca7311b6e8479accf
duanluan <duanluan@outlook.com>
2022-01-13 13:44:10 +0800

    feat: SecurityUtils 新增 DES 算法 CBC 模式加解密的方法

--- src/test/java/top/zhogjianhao/SecurityUtilsTest.java
+++ src/test/java/top/zhogjianhao/SecurityUtilsTest.java
@@ -23,9 +23,9 @@ public class SecurityUtilsTest {
     System.out.println(source);
   }
 
-  @DisplayName("DES 加密性能测试")
+  @DisplayName("JDK 和 BouncyCastle 的 DES 算法 ECB 模式加密性能测试")
   @Test
-  void des() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidCipherTextException, InvalidKeySpecException {
+  void desJdkAndBouncyCastle() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidCipherTextException, InvalidKeySpecException {
     byte[] key = ArrayUtils.toBytes(new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'});
     byte[] in = ArrayUtils.toBytes(new char[]{'中'});
 
@@ -46,5 +46,21 @@ public class SecurityUtilsTest {
     bytes = SecurityUtils.desEcb(key, in, true);
     println("BouncyCastle: " + (LocalDateTime.now().get(ChronoField.MILLI_OF_DAY) - startTime));
     System.out.println(ByteUtils.toHexString(bytes));
+  }
+
+  @DisplayName("DES 算法加解密")
+  @Test
+  void des() throws InvalidCipherTextException {
+    String key = "123";
+    String icv = "456";
+    String in = "我爱中国";
+
+    String result = SecurityUtils.desEcb(key, in, true);
+    println("ECB 模式加密：" + result);
+    System.out.println("ECB 模式解密：" + SecurityUtils.desEcb(key, result, false));
+
+    result = SecurityUtils.desCbc(key, icv, in, true);
+    println("CBC 模式加密：" + result);
+    System.out.println("CBC 模式解密：" + SecurityUtils.desCbc(key, icv, result, false));
   }
 }
