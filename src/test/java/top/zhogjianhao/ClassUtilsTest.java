9bbb5e284f30365c25bffc1dc8a6cbd53baa7d6f
duanluan <duanluan@outlook.com>
2022-04-08 05:31:40 +0800

    feat: 新增 Class 工具类和测试，并新增 是否为基本数据类型 的方法

---
+++ src/test/java/top/zhogjianhao/ClassUtilsTest.java
@@ -0,0 +1,20 @@
+package top.zhogjianhao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+@DisplayName("Class 工具类测试")
+public class ClassUtilsTest {
+
+  private void println(Object source) {
+    System.out.println(source);
+  }
+
+  @DisplayName("isBasic：是否为基本数据类型")
+  @Test
+  void isBasic() {
+    println(ClassUtils.isBasic(1, 1.1f, 1.1, true, (byte) 1, (char) 1, (short) 1));
+  }
+}
