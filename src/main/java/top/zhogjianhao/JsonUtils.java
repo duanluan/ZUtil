64eb4bcd28a1038d99f56b085adf722761640aa1
duanluan <duanluan@outlook.com>
2022-04-17 23:31:44 +0800

    feat: JSON 工具类新增 JSON 字符串转集合 方法的重载

--- src/main/java/top/zhogjianhao/JsonUtils.java
+++ src/main/java/top/zhogjianhao/JsonUtils.java
@@ -21,11 +21,11 @@ public class JsonUtils {
    * @param features 序列化特性
    * @return JSON 字符串
    */
-  public static String toJson(@NonNull final Object object, @NonNull final SerializerFeature... features) {
+  public static String toJson(@NonNull final Object object, final SerializerFeature... features) {
     // 设置输出特性
     if (CollectionUtils.sizeIsNotEmpty(features)) {
       // 输出值为 null 的字段
-      return JSON.toJSONString(ArrayUtils.addFirst(features, SerializerFeature.WriteMapNullValue));
+      return JSON.toJSONString(object, ArrayUtils.addFirst(features, SerializerFeature.WriteMapNullValue));
     }
     return JSON.toJSONString(object, SerializerFeature.WriteMapNullValue);
   }
@@ -39,7 +39,7 @@ public class JsonUtils {
    * @param <T>      对象类型
    * @return 对象
    */
-  public static <T> T parseObject(@NonNull final String json, @NonNull final Class<T> clazz, @NonNull final Feature... features) {
+  public static <T> T parseObject(@NonNull final String json, @NonNull final Class<T> clazz, final Feature... features) {
     return JSONObject.parseObject(json, clazz, features);
   }
 
@@ -54,5 +54,17 @@ public class JsonUtils {
    */
   public static <T> List<T> parseArray(@NonNull final String json, @NonNull final Class<T> clazz, @NonNull final ParserConfig parserConfig) {
     return JSON.parseArray(json, clazz, parserConfig);
+  }
+
+  /**
+   * JSON 字符串转集合
+   *
+   * @param json  JSON 字符串
+   * @param clazz 集合元素类型
+   * @param <T>   集合元素类型
+   * @return 集合
+   */
+  public static <T> List<T> parseArray(@NonNull final String json, @NonNull final Class<T> clazz) {
+    return JSON.parseArray(json, clazz);
   }
 }
