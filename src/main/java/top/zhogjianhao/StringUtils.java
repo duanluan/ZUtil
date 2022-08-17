ee73c4d43f812e455a0808b4a40fcbd0f7eeb43c
duanluan <duanluan@outlook.com>
2022-08-17 14:52:24 +0800

    feat: `StringUtils`新增`首字母大写`的方法；`toUnderscore(String)`和`nCopies(String, long)`如果`source`参数为空时返回`source`而不是返回 null。

--- src/main/java/top/zhogjianhao/StringUtils.java
+++ src/main/java/top/zhogjianhao/StringUtils.java
@@ -98,7 +98,7 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
    */
   public static String toUnderscore(@NonNull final String source) {
     if (isBlank(source)) {
-      return null;
+      return source;
     }
     StringBuilder sb = new StringBuilder(source);
     int temp = 0;
@@ -116,6 +116,21 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
   }
 
   /**
+   * 首字母大写
+   *
+   * @param source 字符串
+   * @return 首字母大写
+   */
+  public static String toInitialUpperCase(@NonNull final String source) {
+    if (isBlank(source)) {
+      return source;
+    }
+    char[] chars = source.toCharArray();
+    chars[0] = Character.toUpperCase(chars[0]);
+    return String.valueOf(chars);
+  }
+
+  /**
    * 字符串重复 n 遍
    *
    * @param source 字符串
@@ -124,7 +139,7 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
    */
   public static String nCopies(@NonNull final String source, final long n) {
     if (isBlank(source) || n < 1) {
-      return null;
+      return source;
     }
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < n; i++) {
