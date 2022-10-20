958d91e5ab80347633ad455320f760645f2b9e3c
duanluan <duanluan@outlook.com>
2022-10-20 17:06:13 +0800

    feat: 新增 java.lang.System 工具类

---
+++ src/main/java/top/zhogjianhao/SystemUtils.java
@@ -0,0 +1,16 @@
+package top.zhogjianhao;
+
+/**
+ * {@code java.lang.System} 工具类
+ */
+public class SystemUtils extends org.apache.commons.lang3.SystemUtils {
+
+  /**
+   * 获取临时目录路径
+   *
+   * @return 临时目录路径
+   */
+  public static String getTempDir() {
+    return getJavaIoTmpDir().getPath();
+  }
+}
