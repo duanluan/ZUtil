71ed3d0dcf387bda8301b1572e822181ccad23d0
Sherman Chu <yeliuleet@gmail.com>
2022-10-30 00:08:35 +0800

    feat: add NanoIDUtils for random ID generation (#10)
    * fix: exclude 'target' folder from vcs
    
    * feat: add NanoIDUtils for random ID generation
    
    * feat(test): add Tests for NanoIDUtils
    
    * refactor: remove redundant author info from file header

---
+++ src/test/java/top/zhogjianhao/junit/id/NanoIDUtilsTest.java
@@ -0,0 +1,40 @@
+package top.zhogjianhao.junit.id;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import top.zhogjianhao.id.NanoIDUtils;
+
+/**
+ * NanoIDUtils 测试类
+ */
+@DisplayName("NanoIDUtils 测试")
+class NanoIDUtilsTest {
+
+    @Test
+    @DisplayName("生成随机 ID (默认)")
+    void randomNanoId() {
+        String id = NanoIDUtils.randomNanoId();
+        System.out.println(id);
+    }
+
+    @Test
+    @DisplayName("生成随机 ID (指定长度)")
+    void testRandomNanoId() {
+        String id = NanoIDUtils.randomNanoId(24);
+        System.out.println(id);
+    }
+
+    @Test
+    @DisplayName("生成随机 ID (指定长度和字典)")
+    void testRandomNanoId1() {
+        String id = NanoIDUtils.randomNanoId(24, "0123456789");
+        System.out.println(id);
+    }
+
+    @Test
+    @DisplayName("生成随机 ID (指定长度、字典和随机数生成器)")
+    void testRandomNanoId2() {
+        String id = NanoIDUtils.randomNanoId(24, "0123456789", NanoIDUtils.DEFAULT_ID_GENERATOR);
+        System.out.println(id);
+    }
+}
