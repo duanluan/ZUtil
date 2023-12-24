82d9d821e02756b30c668ac526fcc043010d0f49
duanluan <duanluan@outlook.com>
2023-12-24 15:27:58 +0800

    refactor: RegExUtils 删除有 Pattern 形参的方法，新增 replaceFirst 替换第一个匹配项的第一个捕获组的匹配值的重载方法

--- src/main/java/top/csaf/regex/RegExUtils.java
+++ src/main/java/top/csaf/regex/RegExUtils.java
@@ -14,6 +14,8 @@ import java.util.regex.Pattern;
 
 /**
  * 正则匹配工具类
+ * <p>
+ * 注意：没有捕获组时则为匹配项本身
  */
 @Slf4j
 public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
@@ -110,21 +112,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 获取下标
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @return 匹配的下标
-   */
-  public static int indexOf(@NonNull final CharSequence text, @NonNull final Pattern pattern) {
-    Matcher matcher = pattern.matcher(text);
-    if (matcher.find()) {
-      return matcher.start();
-    }
-    return -1;
-  }
-
-  /**
    * 获取指定匹配项的指定捕获组的匹配值
    *
    * @param regex 正则
@@ -181,34 +168,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 获取指定匹配项的指定捕获组的匹配值
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @param item    匹配项
-   * @param group   捕获组
-   * @return 匹配值
-   */
-  public static String match(@NonNull final CharSequence text, @NonNull final Pattern pattern, final int item, final int group) {
-    if (item < 0) {
-      throw new IllegalArgumentException("Item: should be greater than 0");
-    }
-    if (group < 0) {
-      throw new IllegalArgumentException("Group: should be greater than 0");
-    }
-    String result = null;
-    Matcher matcher = pattern.matcher(text);
-    int i = 0;
-    while (matcher.find()) {
-      if (item == i) {
-        return matcher.group(group);
-      }
-      i++;
-    }
-    return result;
-  }
-
-  /**
    * 获取指定匹配项的第一个捕获组的匹配值
    *
    * @param text  需要匹配的内容
@@ -218,18 +177,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
    */
   public static String match(@NonNull final CharSequence text, @NonNull final String regex, final int item) {
     return match(text, regex, item, 0);
-  }
-
-  /**
-   * 获取指定匹配项的第一个捕获组的匹配值
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @param item    匹配项
-   * @return 匹配值
-   */
-  public static String match(@NonNull final CharSequence text, @NonNull final Pattern pattern, final int item) {
-    return match(text, pattern, item, 0);
   }
 
   /**
@@ -272,18 +219,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 获取第一个匹配项的指定捕获组的匹配值
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @param group   捕获组
-   * @return 匹配值
-   */
-  public static String matchFirst(@NonNull final CharSequence text, @NonNull final Pattern pattern, final int group) {
-    return match(text, pattern, 0, group);
-  }
-
-  /**
    * 获取第一个匹配项的第一个捕获组的匹配值
    *
    * @param text  需要匹配的内容
@@ -292,17 +227,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
    */
   public static String matchFirst(@NonNull final CharSequence text, @NonNull final String regex) {
     return matchFirst(text, regex, 0);
-  }
-
-  /**
-   * 获取第一个匹配项的第一个捕获组的匹配值
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @return 匹配值
-   */
-  public static String matchFirst(@NonNull final CharSequence text, @NonNull final Pattern pattern) {
-    return matchFirst(text, pattern, 0);
   }
 
   /**
@@ -339,19 +263,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   public static boolean isMatch(@NonNull final CharSequence text, @NonNull final String regex) {
     return Pattern.compile(regex).matcher(text).find();
   }
-
-
-  /**
-   * 是否能匹配
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @return 是否能匹配
-   */
-  public static boolean isMatch(@NonNull final CharSequence text, @NonNull final Pattern pattern) {
-    return pattern.matcher(text).find();
-  }
-
 
   /**
    * 获取指定捕获组的匹配项集合
@@ -397,23 +308,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 获取指定捕获组的匹配项集合
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @param group   捕获组
-   * @return 匹配集合
-   */
-  public static List<String> matches(@NonNull final CharSequence text, @NonNull final Pattern pattern, final int group) {
-    List<String> resultList = new ArrayList<>();
-    Matcher matcher = pattern.matcher(text);
-    while (matcher.find()) {
-      resultList.add(matcher.group(group));
-    }
-    return resultList;
-  }
-
-  /**
    * 获取第一个捕获组的匹配项集合
    *
    * @param text  需要匹配的内容
@@ -422,17 +316,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
    */
   public static List<String> matches(@NonNull final CharSequence text, @NonNull final String regex) {
     return matches(text, regex, 0);
-  }
-
-  /**
-   * 获取第一个捕获组的匹配项集合
-   *
-   * @param text    需要匹配的内容
-   * @param pattern 正则
-   * @return 匹配集合
-   */
-  public static List<String> matches(@NonNull final CharSequence text, @NonNull final Pattern pattern) {
-    return matches(text, pattern, 0);
   }
 
   /**
@@ -521,32 +404,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
     return replace(text, regex, replacement, item, 0);
   }
 
-
-  /**
-   * 替换指定匹配项的第一个捕获组的匹配值
-   *
-   * @param text        需要替换的内容
-   * @param pattern     正则
-   * @param replacement 替换值
-   * @param item        匹配项
-   * @return 替换后的内容
-   */
-  public static String replace(@NonNull final String text, @NonNull final Pattern pattern, @NonNull final String replacement, final int item) {
-    if (item < 0) {
-      throw new IllegalArgumentException("Item: should be greater than 0");
-    }
-    Matcher matcher = pattern.matcher(text);
-    int i = 0;
-    while (matcher.find()) {
-      if (i == item) {
-        int startIndex = matcher.start(0);
-        return text.substring(0, startIndex) + replacement + text.substring(startIndex + matcher.group(0).length());
-      }
-      i++;
-    }
-    return null;
-  }
-
   /**
    * 替换第一个匹配项的指定捕获组的匹配值
    *
@@ -589,19 +446,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 替换第一个匹配项的指定捕获组的匹配值
-   *
-   * @param text        需要替换的内容
-   * @param regex       正则
-   * @param replacement 替换值
-   * @param group       捕获组
-   * @return 替换后的内容
-   */
-  public static String replaceFirst(@NonNull final String text, @NonNull final Pattern regex, @NonNull final String replacement, final int group) {
-    return replace(text, regex, replacement, group);
-  }
-
-  /**
    * 替换第一个匹配项的第一个捕获组的匹配值
    *
    * @param text        需要替换的内容
@@ -609,20 +453,8 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
    * @param replacement 替换值
    * @return 替换后的内容
    */
-  public static String replace(@NonNull final String text, @NonNull final String regex, @NonNull final String replacement) {
+  public static String replaceFirst(@NonNull final String text, @NonNull final String regex, @NonNull final String replacement) {
     return replaceFirst(text, regex, replacement, 0, 0);
-  }
-
-  /**
-   * 替换第一个匹配项的第一个捕获组的匹配值
-   *
-   * @param text        需要替换的内容
-   * @param pattern     正则
-   * @param replacement 替换值
-   * @return 替换后的内容
-   */
-  public static String replace(@NonNull final String text, @NonNull final Pattern pattern, @NonNull final String replacement) {
-    return replaceFirst(text, pattern, replacement, 0);
   }
 
   /**
@@ -707,56 +539,15 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
   }
 
   /**
-   * 替换所有匹配项的指定捕获组的匹配值
+   * 替换所有匹配项的第一个捕获组的匹配值
    *
    * @param text        需要替换的内容
-   * @param pattern     正则
+   * @param regex       正则
    * @param replacement 替换值
-   * @param group       捕获组
    * @return 替换后的内容
    */
-  public static String replaceAll(@NonNull final String text, @NonNull final Pattern pattern, @NonNull final String replacement, final int group) {
-    if (group < 0) {
-      throw new IllegalArgumentException("Group: should be greater than 0");
-    }
-    StringBuilder result = new StringBuilder();
-
-    Matcher matcher = pattern.matcher(text);
-    if (matcher.find()) {
-      // 首次匹配
-      int startIndex = matcher.start(group);
-      String firstMatchText = text.substring(0, startIndex) + replacement + text.substring(startIndex + matcher.group(group).length());
-      result.append(firstMatchText, 0, startIndex + replacement.length());
-      int i = 0;
-
-      while (true) {
-        // 先截取掉已经替换过内容的捕获组
-        String text1 = firstMatchText.substring(result.toString().length());
-        // 然后再去匹配
-        Matcher matcher1 = pattern.matcher(text1);
-        if (matcher1.find()) {
-          int startIndex1 = matcher1.start(group);
-          String nextMatchText = text1.substring(0, startIndex1) + replacement + text1.substring(startIndex1 + matcher1.group(group).length());
-          // 匹配后更新条件，以便下一次循环
-          firstMatchText = result + nextMatchText;
-          result.append(nextMatchText, 0, startIndex1 + replacement.length());
-          i++;
-        } else {
-          break;
-        }
-      }
-
-      // 获取最后一次匹配的结尾文本
-      matcher = pattern.matcher(text);
-      int j = 0;
-      while (matcher.find()) {
-        if (i == j) {
-          result.append(text.substring(matcher.start(group) + 1));
-        }
-        j++;
-      }
-    }
-    return result.toString();
+  public static String replaceAll(@NonNull final String text, @NonNull final String regex, @NonNull final String replacement) {
+    return replaceAll(text, regex, replacement, 0, 0);
   }
 
   /**
@@ -766,6 +557,6 @@ public class RegExUtils extends org.apache.commons.lang3.RegExUtils {
    * @return 是否包含汉字
    */
   public static boolean containsHanZi(@NonNull final String source) {
-    return isMatch(source, CommonPattern.MULTIPLE_CHINESE_CHAR);
+    return isMatch(source, CommonPattern.MULTIPLE_CHINESE_CHAR.pattern());
   }
 }
