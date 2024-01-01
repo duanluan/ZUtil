05b06df049bc83dcfb9e8de847791c39bfe2abad
duanluan <duanluan@outlook.com>
2024-01-01 16:44:19 +0800

    feat: lang.NumberUtil 中 leThanZero、leThanEqToZero、geThanZero、geThanEqToZero、isInteger 方法的形参为 null 时返回 false

--- src/main/java/top/csaf/lang/NumberUtil.java
+++ src/main/java/top/csaf/lang/NumberUtil.java
@@ -9,57 +9,72 @@ import lombok.extern.slf4j.Slf4j;
 public class NumberUtil extends org.apache.commons.lang3.math.NumberUtils {
 
   /**
-   * 小于 0
+   * 小于 0，数字为 null 时返回 false
    *
    * @param number 数字
    * @return 是否小于 0
    */
   public static boolean leThanZero(Number number) {
+    if (number == null) {
+      return false;
+    }
     return number.intValue() < 0;
   }
 
   /**
-   * 小于等于 0
+   * 小于等于 0，数字为 null 时返回 false
    *
    * @param number 数字
    * @return 是否小于等于 0
    */
   public static boolean leThanEqToZero(Number number) {
+    if (number == null) {
+      return false;
+    }
     return number.doubleValue() <= 0;
   }
 
   /**
-   * 大于 0
+   * 大于 0，数字为 null 时返回 false
    *
    * @param number 数字
    * @return 是否大于 0
    */
   public static boolean geThanZero(Number number) {
+    if (number == null) {
+      return false;
+    }
     return number.doubleValue() > 0;
   }
 
   /**
-   * 大于等于 0
+   * 大于等于 0，数字为 null 时返回 false
    *
    * @param number 数字
    * @return 是否大于等于 0
    */
   public static boolean geThanEqToZero(Number number) {
+    if (number == null) {
+      return false;
+    }
     return number.doubleValue() >= 0;
   }
 
   /**
-   * 是否为整数
+   * 是否为整数，字符串为 null 时返回 false
    *
    * @param str 字符串
    * @return 是否为整数
    */
   public static boolean isInteger(String str) {
+    if (StrUtil.isBlank(str)) {
+      return false;
+    }
     return str.matches("^[-+]?[\\d]*$");
   }
 
   /**
-   * 解析为 Long
+   * 字符串转 Long，解析失败时为 0L
    *
    * @param s 字符串
    * @return Long 类型变量
