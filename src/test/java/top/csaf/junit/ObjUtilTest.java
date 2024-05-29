25065d07436366c5777796a379d121c80d9a20e4
RuKai <1913862454@qq.com>
2024-05-29 10:52:46 +0800

    test: 新增ObjrUtil工具类测试

---
+++ src/test/java/top/csaf/junit/ObjUtilTest.java
@@ -0,0 +1,33 @@
+package top.csaf.junit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import top.csaf.lang.ObjUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Description Object 工具类测试
+ * @Author Rick
+ * @Date 2024/5/29 10:34
+ **/
+@Slf4j
+@DisplayName("NUMBER 工具类测试")
+public class ObjUtilTest {
+
+    @DisplayName("isAllEquals：小于 0，数字为 null 时返回 false")
+    @Test
+    void isAllEquals() {
+        assertTrue(ObjUtil.isAllEquals(false, true, "test", null, null));
+        // TODO 此处抛出空指针异常
+        // assertFalse(ObjUtil.isAllEquals(false, false, "test", null, null));
+        assertFalse(ObjUtil.isAllEquals(false, false, "test", 123, 123.0));
+        assertFalse(ObjUtil.isAllEquals(true, false, "test", 123, 123.0));
+        assertFalse(ObjUtil.isAllEquals(true, true, "test", 123, 123.0, null));
+        assertTrue(ObjUtil.isAllEquals(false, false, "test", "test", "test"));
+        assertTrue(ObjUtil.isAllEquals(true, false, "test", "test", "test"));
+        assertFalse(ObjUtil.isAllEquals(true, false, "test", 123, 123.0));
+        assertThrows(IllegalArgumentException.class, () -> ObjUtil.isAllEquals(false, false, "test"));
+    }
+}
