2d494c61849a15bd5d8d28be9e89ea0d822c4d70
duanluan <duanluan@outlook.com>
2024-08-22 10:55:00 +0800

    feat: MD5 工具类新增加密 byte 数组的方法，加密方法输入为 null 或空字符串时返回空字符串

--- zutil-all/src/main/java/top/csaf/crypto/Md5Util.java
+++ zutil-all/src/main/java/top/csaf/crypto/Md5Util.java
@@ -1,6 +1,7 @@
 package top.csaf.crypto;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import top.csaf.lang.StrUtil;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -15,11 +16,18 @@ public class Md5Util {
   /**
    * MD5 加密
    *
-   * @param in          输入
+   * @param in          输入，可以是 String 或 byte[]
    * @param isUpperCase 是否转大写
    * @return 加密后的字符串
    */
-  public static String to(String in, boolean isUpperCase) {
+  private static String to(Object in, boolean isUpperCase) {
+    if (StrUtil.isBlank(in)) {
+      return "";
+    }
+    if (!(in instanceof String) && !(in instanceof byte[])) {
+      throw new IllegalArgumentException("in must be String or byte[]");
+    }
+
     Security.addProvider(new BouncyCastleProvider());
     MessageDigest md;
     try {
@@ -27,12 +35,13 @@ public class Md5Util {
     } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
       throw new RuntimeException(e);
     }
-    byte[] messageDigest = md.digest(in.getBytes());
+    byte[] messageDigest = md.digest(in instanceof String ? ((String) in).getBytes() : (byte[]) in);
     StringBuilder hexString = new StringBuilder();
 
     for (byte b : messageDigest) {
       String hex = Integer.toHexString(0xff & b);
-      if (hex.length() == 1) {
+      // 保证生成的十六进制字符串长度为32位（每个字节转换后的十六进制是两位才行），需要在长度为1的十六进制字符串前补充0
+      if (in instanceof byte[] && hex.length() == 1) {
         hexString.append('0');
       }
       hexString.append(hex);
@@ -49,7 +58,7 @@ public class Md5Util {
   /**
    * MD5 加密后转大写
    *
-   * @param in 输入
+   * @param in 需要加密的字符串
    * @return 加密后的字符串
    */
   public static String toUpperCase(String in) {
@@ -59,7 +68,7 @@ public class Md5Util {
   /**
    * MD5 加密（小写）
    *
-   * @param in 输入
+   * @param in 需要加密的字符串
    * @return 加密后的字符串
    */
   public static String toLowerCase(String in) {
@@ -69,7 +78,7 @@ public class Md5Util {
   /**
    * MD5 加密后截取中间 16 位并转大写
    *
-   * @param in 输入
+   * @param in 需要加密的字符串
    * @return 加密后的字符串
    */
   public static String toUpperCaseShort(String in) {
@@ -79,10 +88,50 @@ public class Md5Util {
   /**
    * MD5 加密后截取中间 16 位（小写）
    *
-   * @param in 输入
+   * @param in 需要加密的字符串
    * @return 加密后的字符串
    */
   public static String toLowerCaseShort(String in) {
+    return to(in, false).substring(8, 24);
+  }
+
+  /**
+   * MD5 加密后转大写
+   *
+   * @param in 需要加密的 byte 数组
+   * @return 加密后的字符串
+   */
+  public static String toUpperCase(byte[] in) {
+    return to(in, true);
+  }
+
+  /**
+   * MD5 加密（小写）
+   *
+   * @param in 需要加密的 byte 数组
+   * @return 加密后的字符串
+   */
+  public static String toLowerCase(byte[] in) {
+    return to(in, false);
+  }
+
+  /**
+   * MD5 加密后截取中间 16 位并转大写
+   *
+   * @param in 需要加密的 byte 数组
+   * @return 加密后的字符串
+   */
+  public static String toUpperCaseShort(byte[] in) {
+    return to(in, true).substring(8, 24);
+  }
+
+  /**
+   * MD5 加密后截取中间 16 位（小写）
+   *
+   * @param in 需要加密的 byte 数组
+   * @return 加密后的字符串
+   */
+  public static String toLowerCaseShort(byte[] in) {
     return to(in, false).substring(8, 24);
   }
 }
--- zutil-all/src/test/java/top/csaf/junit/Md5UtilTest.java
+++ zutil-all/src/test/java/top/csaf/junit/Md5UtilTest.java
@@ -3,40 +3,48 @@ package top.csaf.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import top.csaf.crypto.Md5Util;
+import top.csaf.io.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @Description MD5工具类测试
- * @Author Rick
- * @Date 2024/6/13 9:49
- **/
-@DisplayName("MD5工具类测试")
+ * MD5 工具类测试
+ */
+@DisplayName("MD5 工具类测试")
 public class Md5UtilTest {
 
 
-    @DisplayName("MD5 加密")
-    @Test
-    void to() {
-        // 大写转换
-        String input = "hello";
-        String expected = "5D41402ABC4B2A76B9719D911017C592";
-        String upperActual = Md5Util.to(input, true);
-        assertEquals(expected, upperActual, "The MD5 hash of 'hello' in uppercase should match the expected value.");
-        // 小写转换
-        expected = "5d41402abc4b2a76b9719d911017c592";
-        String lowerActual = Md5Util.to(input, false);
-        assertEquals(expected, lowerActual, "The MD5 hash of 'hello' in lowercase should match the expected value.");
-        // 空字符转换
-        input = "";
-        String expectedUpperCase = "D41D8CD98F00B204E9800998ECF8427E";
-        String expectedLowerCase = "d41d8cd98f00b204e9800998ecf8427e";
-        String actualUpperCase = Md5Util.to(input, true);
-        assertEquals(expectedUpperCase, actualUpperCase, "The MD5 hash of an empty string in uppercase should match the expected value.");
-        String actualLowerCase = Md5Util.to(input, false);
-        assertEquals(expectedLowerCase, actualLowerCase, "The MD5 hash of an empty string in lowercase should match the expected value.");
-        // null值抛出异常
-        assertThrows(NullPointerException.class, () -> Md5Util.to(null, true), "Passing null to the to() method should throw a NullPointerException.");
-        assertThrows(NullPointerException.class, () -> Md5Util.to(null, false), "Passing null to the to() method should throw a NullPointerException.");
-    }
+  @DisplayName("MD5 加密")
+  @Test
+  void to() throws IOException {
+    // 字符串类型
+    String input = "hello";
+    // 大写
+    assertEquals("5D41402ABC4B2A76B9719D911017C592", Md5Util.toUpperCase(input));
+    // 大写短写（中间 16 位）
+    assertEquals("BC4B2A76B9719D91", Md5Util.toUpperCaseShort(input));
+    // 小写
+    assertEquals("5d41402abc4b2a76b9719d911017c592", Md5Util.toLowerCase(input));
+    // 小写短写（中间 16 位）
+    assertEquals("bc4b2a76b9719d91", Md5Util.toLowerCaseShort(input));
+
+    // byte[] 类型
+    byte[] bytes = FileUtil.readFileToByteArray(new File("C:\\Windows\\notepad.exe"));
+    // 大写
+    assertEquals("FF3E29FDFAFA0E9030E2FCD71489D41D", Md5Util.toUpperCase(bytes));
+    // 大写短写（中间 16 位）
+    assertEquals("FAFA0E9030E2FCD7", Md5Util.toUpperCaseShort(bytes));
+    // 小写
+    assertEquals("ff3e29fdfafa0e9030e2fcd71489d41d", Md5Util.toLowerCase(bytes));
+    // 小写短写（中间 16 位）
+    assertEquals("fafa0e9030e2fcd7", Md5Util.toLowerCaseShort(bytes));
+
+    // null
+    assertEquals("", Md5Util.toUpperCase((String) null));
+    assertEquals("", Md5Util.toUpperCase((byte[]) null));
+    assertEquals("", Md5Util.toUpperCase(""));
+  }
 }
