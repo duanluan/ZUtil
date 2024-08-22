3522ba8f0a2555bd129cfd4496df26b589217526
duanluan <duanluan@outlook.com>
2024-08-22 16:36:52 +0800

    feat: MapUtil 新增 org.apache.commons.collections4.MapUtils 所有方法

--- zutil-all/src/test/java/top/csaf/junit/MapUtilTest.java
+++ zutil-all/src/test/java/top/csaf/junit/MapUtilTest.java
@@ -3,8 +3,12 @@ package top.csaf.junit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.DefaultParameterNameDiscoverer;
 import top.csaf.coll.MapUtil;
+import top.csaf.lang.StrUtil;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,25 +23,61 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("MapUtil 工具类测试")
 public class MapUtilTest {
 
-    @DisplayName("isEmpty：是否为空Map")
-    @Test
-    void isEmpty() {
-        assertTrue(MapUtil.isEmpty(null));
-        Map<Object, Object> map = new HashMap<>();
-        assertTrue(MapUtil.isEmpty(map));
-        map.put("key", "value");
-        assertFalse(MapUtil.isEmpty(map));
+  public static void main(String[] args) {
+    genMapUtilsFn();
+  }
+
+  /**
+   * 调用 {@link org.apache.commons.collections4.MapUtils } 的方法
+   */
+  static void genMapUtilsFn() {
+    // 获取 org.apache.commons.collections4.MapUtils 中所有的方法名
+    Class<?> clazz = org.apache.commons.collections4.MapUtils.class;
+    Method[] methods = clazz.getDeclaredMethods();
+    // 根据方法生成调用方法代码
+    for (Method method : methods) {
+      // 修饰符不为 public static 时跳过
+      if (method.getModifiers() != (Modifier.PUBLIC | Modifier.STATIC)) {
+        continue;
+      }
+      // 有 @Deprecated 注解时跳过
+      if (method.getAnnotation(Deprecated.class) != null) {
+        continue;
+      }
+      // 参数名
+      String[] paramNames = new DefaultParameterNameDiscoverer().getParameterNames(method);
+
+      // 方法描述，比如 public static <K,V> java.util.Map$Entry<K, V> org.apache.commons.collections4.MapUtils.get(java.util.Map<K, V>,int)
+      String genericStr = method.toGenericString();
+      // TODO 获取方法返回值泛型的继承类，比如 public static <O extends Comparable<? super O>> List<O> collate(final Iterable<? extends O> a, final Iterable<? extends O> b) {
+      // 将 java.util.Map$Entry<K, V> 改为 java.util.Map.Entry<K, V>
+      genericStr = genericStr.replaceAll("\\$", ".");
+      // 去除 org.apache.commons.collections4.MapUtils
+      genericStr = genericStr.replace("org.apache.commons.collections4.MapUtils.", "");
+      // 给参数类型加上参数名
+      StringBuilder paramsStr = new StringBuilder();
+      String paramsType = genericStr.substring(genericStr.indexOf("(") + 1, genericStr.indexOf(")"));
+      if (StrUtil.isNotBlank(paramsType)) {
+        String[] paramTypes = paramsType.split(",");
+        int j = 0;
+        for (int i = 0; i < paramTypes.length; i++) {
+          String param = paramTypes[i];
+          // 如果为泛型，当前参数为当前参数 + 下一个参数
+          if (param.contains("<") && !param.contains(">")) {
+            param = param + "," + paramTypes[i + 1];
+            i++;
+          }
+          paramsStr.append(param).append(" ").append(paramNames[j]);
+          if (i != paramTypes.length - 1) {
+            paramsStr.append(", ");
+          }
+          j++;
+        }
+      }
+      // 生成代码
+      System.out.println(genericStr.replace(genericStr.substring(genericStr.indexOf("("), genericStr.indexOf(")") + 1), "(" + paramsStr + ")") + " {");
+      System.out.println("  " + (method.getReturnType().getSimpleName().equals("void") ? "" : "return ") + "org.apache.commons.collections4.MapUtils." + method.getName() + "(" + StrUtil.join(paramNames, ", ") + ");");
+      System.out.println("}\n");
     }
-
-    @DisplayName("isNotEmpty：是否不为空Map")
-    @Test
-    void isNotEmpty() {
-        assertFalse(MapUtil.isNotEmpty(null));
-        Map<Object, Object> map = new HashMap<>();
-        assertFalse(MapUtil.isNotEmpty(map));
-        map.put("key", "value");
-        assertTrue(MapUtil.isNotEmpty(map));
-    }
-
-
+  }
 }
--- zutil-core/src/main/java/top/csaf/coll/MapUtil.java
+++ zutil-core/src/main/java/top/csaf/coll/MapUtil.java
@@ -4,11 +4,279 @@ import java.util.Map;
 
 public class MapUtil {
 
-  public static boolean isEmpty(final Map<?,?> map) {
+  // region org.apache.commons.collections4.MapUtils
+
+  public static <K> java.lang.Boolean getBoolean(java.util.Map<? super K, ?> map, K key, java.lang.Boolean defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getBoolean(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Boolean getBoolean(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getBoolean(map, key);
+  }
+
+  public static <K> java.lang.Byte getByte(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getByte(map, key);
+  }
+
+  public static <K> java.lang.Byte getByte(java.util.Map<? super K, ?> map, K key, java.lang.Byte defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getByte(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Short getShort(java.util.Map<? super K, ?> map, K key, java.lang.Short defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getShort(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Short getShort(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getShort(map, key);
+  }
+
+  public static <K> java.lang.Long getLong(java.util.Map<? super K, ?> map, K key, java.lang.Long defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getLong(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Long getLong(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getLong(map, key);
+  }
+
+  public static <K> java.lang.Float getFloat(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getFloat(map, key);
+  }
+
+  public static <K> java.lang.Float getFloat(java.util.Map<? super K, ?> map, K key, java.lang.Float defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getFloat(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Double getDouble(java.util.Map<? super K, ?> map, K key, java.lang.Double defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getDouble(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Double getDouble(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getDouble(map, key);
+  }
+
+  public static boolean isEmpty(java.util.Map<?, ?> map) {
     return org.apache.commons.collections4.MapUtils.isEmpty(map);
   }
 
-  public static boolean isNotEmpty(final Map<?,?> map) {
+  public static int size(java.util.Map<?, ?> map) {
+    return org.apache.commons.collections4.MapUtils.size(map);
+  }
+
+  public static <K,V> java.util.Map<K, V> putAll(java.util.Map<K, V> map, java.lang.Object[] array) {
+    return org.apache.commons.collections4.MapUtils.putAll(map, array);
+  }
+
+  public static java.util.Map<java.lang.String, java.lang.Object> toMap(java.util.ResourceBundle resourceBundle) {
+    return org.apache.commons.collections4.MapUtils.toMap(resourceBundle);
+  }
+
+  public static <K,V> V getObject(java.util.Map<? super K, V> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getObject(map, key);
+  }
+
+  public static <K,V> V getObject(java.util.Map<K, V> map, K key, V defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getObject(map, key, defaultValue);
+  }
+
+  public static <K> java.lang.Integer getInteger(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getInteger(map, key);
+  }
+
+  public static <K> java.lang.Integer getInteger(java.util.Map<? super K, ?> map, K key, java.lang.Integer defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getInteger(map, key, defaultValue);
+  }
+
+  public static <K,V> java.util.Map<K, V> unmodifiableMap(java.util.Map<? extends K, ? extends V> map) {
+    return org.apache.commons.collections4.MapUtils.unmodifiableMap(map);
+  }
+
+  public static <K> java.util.Map<?, ?> getMap(java.util.Map<? super K, ?> map, K key, java.util.Map<?, ?> defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getMap(map, key, defaultValue);
+  }
+
+  public static <K> java.util.Map<?, ?> getMap(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getMap(map, key);
+  }
+
+  public static <K> java.lang.Number getNumber(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getNumber(map, key);
+  }
+
+  public static <K> java.lang.Number getNumber(java.util.Map<? super K, ?> map, K key, java.lang.Number defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getNumber(map, key, defaultValue);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> unmodifiableSortedMap(java.util.SortedMap<K, ? extends V> map) {
+    return org.apache.commons.collections4.MapUtils.unmodifiableSortedMap(map);
+  }
+
+  public static <K,V> java.util.Map<K, V> synchronizedMap(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.synchronizedMap(map);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> synchronizedSortedMap(java.util.SortedMap<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.synchronizedSortedMap(map);
+  }
+
+  public static <K> java.lang.String getString(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getString(map, key);
+  }
+
+  public static <K> java.lang.String getString(java.util.Map<? super K, ?> map, K key, java.lang.String defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getString(map, key, defaultValue);
+  }
+
+  public static boolean isNotEmpty(java.util.Map<?, ?> map) {
     return org.apache.commons.collections4.MapUtils.isNotEmpty(map);
   }
+
+  public static <K,V> java.util.SortedMap<K, V> predicatedSortedMap(java.util.SortedMap<K, V> map, org.apache.commons.collections4.Predicate<? super K> keyPred, org.apache.commons.collections4.Predicate<? super V> valuePred) {
+    return org.apache.commons.collections4.MapUtils.predicatedSortedMap(map, keyPred, valuePred);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> transformedSortedMap(java.util.SortedMap<K, V> map, org.apache.commons.collections4.Transformer<? super K, ? extends K> keyTransformer, org.apache.commons.collections4.Transformer<? super V, ? extends V> valueTransformer) {
+    return org.apache.commons.collections4.MapUtils.transformedSortedMap(map, keyTransformer, valueTransformer);
+  }
+
+  public static <K> boolean getBooleanValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getBooleanValue(map, key);
+  }
+
+  public static <K> boolean getBooleanValue(java.util.Map<? super K, ?> map, K key, boolean defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getBooleanValue(map, key, defaultValue);
+  }
+
+  public static <K> byte getByteValue(java.util.Map<? super K, ?> map, K key, byte defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getByteValue(map, key, defaultValue);
+  }
+
+  public static <K> byte getByteValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getByteValue(map, key);
+  }
+
+  public static <K> short getShortValue(java.util.Map<? super K, ?> map, K key, short defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getShortValue(map, key, defaultValue);
+  }
+
+  public static <K> short getShortValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getShortValue(map, key);
+  }
+
+  public static void verbosePrint(java.io.PrintStream out, java.lang.Object label, java.util.Map<?, ?> map) {
+    org.apache.commons.collections4.MapUtils.verbosePrint(out, label, map);
+  }
+
+  public static void debugPrint(java.io.PrintStream out, java.lang.Object label, java.util.Map<?, ?> map) {
+    org.apache.commons.collections4.MapUtils.debugPrint(out, label, map);
+  }
+
+  public static <K> int getIntValue(java.util.Map<? super K, ?> map, K key, int defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getIntValue(map, key, defaultValue);
+  }
+
+  public static <K> int getIntValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getIntValue(map, key);
+  }
+
+  public static <K> float getFloatValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getFloatValue(map, key);
+  }
+
+  public static <K> float getFloatValue(java.util.Map<? super K, ?> map, K key, float defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getFloatValue(map, key, defaultValue);
+  }
+
+  public static <K> double getDoubleValue(java.util.Map<? super K, ?> map, K key, double defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getDoubleValue(map, key, defaultValue);
+  }
+
+  public static <K> double getDoubleValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getDoubleValue(map, key);
+  }
+
+  public static <K> long getLongValue(java.util.Map<? super K, ?> map, K key) {
+    return org.apache.commons.collections4.MapUtils.getLongValue(map, key);
+  }
+
+  public static <K> long getLongValue(java.util.Map<? super K, ?> map, K key, long defaultValue) {
+    return org.apache.commons.collections4.MapUtils.getLongValue(map, key, defaultValue);
+  }
+
+  public static <K,V> java.util.Properties toProperties(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.toProperties(map);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> predicatedMap(java.util.Map<K, V> map, org.apache.commons.collections4.Predicate<? super K> keyPred, org.apache.commons.collections4.Predicate<? super V> valuePred) {
+    return org.apache.commons.collections4.MapUtils.predicatedMap(map, keyPred, valuePred);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> fixedSizeMap(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.fixedSizeMap(map);
+  }
+
+  public static <K,V> java.util.Map<V, K> invertMap(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.invertMap(map);
+  }
+
+  public static <K> void safeAddToMap(java.util.Map<? super K, java.lang.Object> map, K key, java.lang.Object value) throws java.lang.NullPointerException {
+    org.apache.commons.collections4.MapUtils.safeAddToMap(map, key, value);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> transformedMap(java.util.Map<K, V> map, org.apache.commons.collections4.Transformer<? super K, ? extends K> keyTransformer, org.apache.commons.collections4.Transformer<? super V, ? extends V> valueTransformer) {
+    return org.apache.commons.collections4.MapUtils.transformedMap(map, keyTransformer, valueTransformer);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> lazyMap(java.util.Map<K, V> map, org.apache.commons.collections4.Factory<? extends V> factory) {
+    return org.apache.commons.collections4.MapUtils.lazyMap(map, factory);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> lazyMap(java.util.Map<K, V> map, org.apache.commons.collections4.Transformer<? super K, ? extends V> transformerFactory) {
+    return org.apache.commons.collections4.MapUtils.lazyMap(map, transformerFactory);
+  }
+
+  public static <K,V> org.apache.commons.collections4.OrderedMap<K, V> orderedMap(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.orderedMap(map);
+  }
+
+  public static <K,V> java.util.Map<K, V> emptyIfNull(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.emptyIfNull(map);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> lazySortedMap(java.util.SortedMap<K, V> map, org.apache.commons.collections4.Transformer<? super K, ? extends V> transformerFactory) {
+    return org.apache.commons.collections4.MapUtils.lazySortedMap(map, transformerFactory);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> lazySortedMap(java.util.SortedMap<K, V> map, org.apache.commons.collections4.Factory<? extends V> factory) {
+    return org.apache.commons.collections4.MapUtils.lazySortedMap(map, factory);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableSortedMap<K, V> iterableSortedMap(java.util.SortedMap<K, V> sortedMap) {
+    return org.apache.commons.collections4.MapUtils.iterableSortedMap(sortedMap);
+  }
+
+  public static <K,V,E> void populateMap(org.apache.commons.collections4.MultiMap<K, V> map, java.lang.Iterable<? extends E> elements, org.apache.commons.collections4.Transformer<E, K> keyTransformer, org.apache.commons.collections4.Transformer<E, V> valueTransformer) {
+    org.apache.commons.collections4.MapUtils.populateMap(map, elements, keyTransformer, valueTransformer);
+  }
+
+  public static <K,V> void populateMap(java.util.Map<K, V> map, java.lang.Iterable<? extends V> elements, org.apache.commons.collections4.Transformer<V, K> keyTransformer) {
+    org.apache.commons.collections4.MapUtils.populateMap(map, elements, keyTransformer);
+  }
+
+  public static <K,V,E> void populateMap(java.util.Map<K, V> map, java.lang.Iterable<? extends E> elements, org.apache.commons.collections4.Transformer<E, K> keyTransformer, org.apache.commons.collections4.Transformer<E, V> valueTransformer) {
+    org.apache.commons.collections4.MapUtils.populateMap(map, elements, keyTransformer, valueTransformer);
+  }
+
+  public static <K,V> void populateMap(org.apache.commons.collections4.MultiMap<K, V> map, java.lang.Iterable<? extends V> elements, org.apache.commons.collections4.Transformer<V, K> keyTransformer) {
+    org.apache.commons.collections4.MapUtils.populateMap(map, elements, keyTransformer);
+  }
+
+  public static <K,V> java.util.SortedMap<K, V> fixedSizeSortedMap(java.util.SortedMap<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.fixedSizeSortedMap(map);
+  }
+
+  public static <K,V> org.apache.commons.collections4.IterableMap<K, V> iterableMap(java.util.Map<K, V> map) {
+    return org.apache.commons.collections4.MapUtils.iterableMap(map);
+  }
+
+  // endregion
 }
